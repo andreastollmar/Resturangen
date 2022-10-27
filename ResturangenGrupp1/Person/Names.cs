cf5609f2fb4d24deddab5f73c82c4effbb18dcf2
andreastollmar <115992410+andreastollmar@users.noreply.github.com>
2022-10-27 11:03:45 +0200

    Uppdated nameslist

--- ResturangenGrupp1/Person/Names.cs
+++ ResturangenGrupp1/Person/Names.cs
@@ -8,5 +8,25 @@ namespace ResturangenGrupp1.Person
 {
     internal class Names
     {
+
+
+        public string NameGenerator()
+        {
+            string[] names = {"Andersson", "Larsson", "Svensson", "Pettersson",
+                "Stark", "Soldat", "Bernadotte", "Trump", "Karlsson", "Fridolin", "Löfven", "Eriksson", "Isak", "Ibrahimovic", "Forsberg",
+                "Olsson", "Mosby", "Simpson", "Griffin", "Baggins", "Johanson", "Frid", "Granath", "Grumpycat", "Karen", "C/o Segemyr",
+                "Glans", "Gustafsson", "Tengil", "Lönngren", "Berggren", "Josefsson", "Tollmar", "Aadelen", "Stranden", "Stancic", "Stainaté",
+                "Stamström", "Stömberg", "Stamp", "Stampe", "Stammis", "Stalebäck", "Rasulzada", "Rastgar", "Putin", "Rangu", "Rangenstål",
+                "Rangbo", "Ranemyr", "Ranebo", "Nelleros", "Nellhans", "Nelbäck", "Nekmal", "Nejadi", "Nesse", "Nedrup", "Nedbal", "Nattsjö",
+                "Nathaniel", "Nastrovji", "Bly", "Blum", "Bloom", "Gardell", "Ahlm", "Grenqvist", "Krön", "Stålberg", "Danielsson", "Ekberg",
+                "Fridolin", "Gran", "Hökqvist", "Ivarsson", "Jansson", "Knutsdottrí", "Lavin", "Mjolnir", "Nedved", "Oscarsdottrí", "Pistvakt",
+                "Qrench", "Zeon", "Xerxes", "Yves", "Ålström", "Ängmark", "Östman"};
+
+            Random rnd = new Random();
+            int randomName = rnd.Next(0, names.Length);
+            return names[randomName];
+        }
+
+
     }
 }
