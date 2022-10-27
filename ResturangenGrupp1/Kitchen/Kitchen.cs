e2e93a304aefefa1224058701b7e2be5d05fc040
MohammadAlzuabi <115696615+MohammadAlzuabi@users.noreply.github.com>
2022-10-27 13:31:40 +0200

    Trying to do kitchen :leopard:

--- ResturangenGrupp1/Kitchen/Kitchen.cs
+++ ResturangenGrupp1/Kitchen/Kitchen.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ResturangenGrupp1.Person;
+using ResturangenGrupp1.Restaurant;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,24 @@ namespace ResturangenGrupp1.Kitchen
 {
     internal class Kitchen
     {
+        Food food = new Food();
+        public int Time { get; set; } = 10;
+        public int Kock { get; set; } = 5;
+
+
+        public Kitchen(int time, int kock)
+        {
+            Time = time;
+            Kock = kock;
+        }
+
+        public void CookFood(List<Food> meny)
+        {
+     
+        }
+
+       
+
+
     }
 }
