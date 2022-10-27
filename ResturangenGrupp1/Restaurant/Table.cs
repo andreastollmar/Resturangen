5c79c9ce779dd785bcb233fdc0feafe940437b46
andreastollmar <115992410+andreastollmar@users.noreply.github.com>
2022-10-27 11:29:26 +0200

    Update on TableClass, Interface Try

--- ResturangenGrupp1/Restaurant/Table.cs
+++ ResturangenGrupp1/Restaurant/Table.cs
@@ -6,7 +6,63 @@ using System.Threading.Tasks;
 
 namespace ResturangenGrupp1.Restaurant
 {
-    internal class Table
+    interface ITable
     {
+        
+        bool Quality { get; set; }
+        bool Empty { get; set; }
+        bool Cleaned { get; set; }
+        bool GetsHelp { get; set; }
+        bool RandomQuality();
     }
+    class TableForTwo : ITable
+    {        
+        public bool Quality { get; set; }
+        public bool Empty { get; set; }
+        public bool Cleaned { get; set; }
+        public bool GetsHelp { get; set; }
+
+        public bool RandomQuality()
+        {
+            bool goodQuality = false;
+            Random rnd = new Random();
+            int randomQuality = rnd.Next(0, 100);
+            if (randomQuality >= 60)
+            {
+                goodQuality = true;
+            }
+            return goodQuality;
+        }
+        public TableForTwo()
+        {
+            string[] tableSize = new string[2];
+            Quality = RandomQuality();
+        }
+    }
+    class TableForFour : ITable
+    {        
+        public bool Quality { get; set; }
+        public bool Empty { get; set; }
+        public bool Cleaned { get; set; }
+        public bool GetsHelp { get; set; }
+        public bool RandomQuality()
+        {
+            bool goodQuality = false;
+            Random rnd = new Random();
+            int randomQuality = rnd.Next(0, 100);
+            if (randomQuality >= 60)
+            {
+                goodQuality = true;
+            }
+            return goodQuality;
+        }
+        public TableForFour()
+        {
+            string[] tableSize = new string[4];
+            Quality = RandomQuality();
+        }
+    }
+
+
+
 }
