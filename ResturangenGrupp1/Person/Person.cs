638f61e848a85cf97a1078acb92f36878650a077
Jonatan Ardensten <jonatan.ardensten@gmail.com>
2022-10-27 11:30:27 +0200

    Added person class and subclasses

--- ResturangenGrupp1/Person/Person.cs
+++ ResturangenGrupp1/Person/Person.cs
@@ -1,8 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace ResturangenGrupp1.Person
 {
-	public Class1()
-	{
-	}
+    internal class Person
+    {
+        internal string Name { get; set; }
+        internal int Competence { get; set; }
+        internal int TimeActivity { get; set; }
+
+        public int RandomCompetence()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 6);
+        }
+
+        public Person()
+        {
+            Name = "";
+            TimeActivity = 0;
+            Competence = 0;
+        }
+    }
+
+    internal class Guest : Person
+    {
+        private int Satisfaction { get; set; }
+        private bool Eating { get; set; }
+
+        private bool NutAllergy { get; set; }
+        private bool LactoseAllergy { get; set; }
+        private bool GlutenAllergy { get; set; }
+
+        private void AtTable()
+        {
+
+        }
+
+        public Guest(): base()
+        {
+            Name = Names.NameGenerator();
+            TimeActivity = 20;
+            NutAllergy = false;
+            LactoseAllergy = false;
+            GlutenAllergy = false;
+        }
+    }
+
+    internal class Waiter : Person
+    {
+        private bool Busy { get; set; }
+
+        private void Cleaning()
+        {
+
+        }
+
+        public Waiter(): base()
+        {
+            Name = Names.NameGenerator();
+            TimeActivity = 3;
+            Busy = false;
+            Competence = RandomCompetence();
+        }
+
+    }
+
+    internal class Chef : Person
+    {
+        private bool Busy { get; set; }
+
+        private void Cooking()
+        {
+
+        }
+
+        public Chef(): base()
+        {
+            Name = Names.NameGenerator();
+            TimeActivity = 10;
+            Busy = false;
+            Competence = RandomCompetence();
+        }
+    }
+
 }
