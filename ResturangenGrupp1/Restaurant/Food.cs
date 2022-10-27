a45ac44f9faa085ed8541348dfe6790b7d3a590d
MohammadAlzuabi <115696615+MohammadAlzuabi@users.noreply.github.com>
2022-10-27 11:13:48 +0200

    Doing food calss

--- ResturangenGrupp1/Restaurant/Food.cs
+++ ResturangenGrupp1/Restaurant/Food.cs
@@ -8,5 +8,80 @@ namespace ResturangenGrupp1.Restaurant
 {
     internal class Food
     {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+
+    }
+
+    class Fich : Food
+    {
+        //public string nameFood1 = "Tuna";
+        //public string nameFood2 = "Scallop";
+        //public string nameFood3 = "Moules frites";
+
+        internal class Tuna : Fich
+        {
+             bool consitns;
+        }
+
+        internal class Scallop : Fich
+        {
+             bool consitns;
+        }
+
+        internal class Moules : Fich
+        {
+             bool consitns;
+        }
+
+    }
+
+    class Meet : Food
+    {
+
+        //public string nameFood1 = "Kebab";
+        //public string nameFood2 = "Sirloin steak";
+        //public string nameFood3 = "Beef plank";
+        internal class Kebab : Meet
+        {
+             bool consitns;
+        }
+
+        internal class Steak : Meet
+        {
+             bool consitns;
+        }
+
+        internal class Beef : Meet
+        {
+             bool consitns;
+        }
+
+
+    }
+
+    class Vegetarian : Food
+    {
+  
+        //public string nameFood1 = "Risotto";
+        //public string nameFood2 = "Falafel";
+        //public string nameFood3 = "Haloumi ";
+
+
+        internal class Risotto : Vegetarian
+        {
+             bool consitns;
+        }
+
+        internal class Falafel : Vegetarian
+        {
+             bool consitns;
+        }
+
+        internal class Haloumi : Vegetarian
+        {
+             bool consitns;
+        }
     }
 }
