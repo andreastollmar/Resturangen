2a5aba96e07624f89664ec8b1c8f1c9d8a9dcd12
andreastollmar <115992410+andreastollmar@users.noreply.github.com>
2022-10-27 11:08:07 +0200

    Random generate Allergi

--- ResturangenGrupp1/Person/Allergies.cs
+++ ResturangenGrupp1/Person/Allergies.cs
@@ -8,5 +8,17 @@ namespace ResturangenGrupp1.Person
 {
     internal class Allergies
     {
+        public bool IsAllergic()
+        {
+            bool allergic = false;
+            Random rnd = new Random();
+            int allergicCheck = rnd.Next(0, 100);
+            if (allergicCheck >= 80)
+            {
+                allergic = true;
+            }
+            return allergic;
+        }
+
     }
 }
