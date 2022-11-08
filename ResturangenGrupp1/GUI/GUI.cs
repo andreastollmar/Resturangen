02f6f137424ad49adca7b1686c8349666ac895ce
Jonatan Ardensten <jonatan.ardensten@gmail.com>
2022-11-08 14:25:17 +0100

    Update GUI.cs

--- ResturangenGrupp1/GUI/GUI.cs
+++ ResturangenGrupp1/GUI/GUI.cs
@@ -247,11 +247,12 @@ namespace ResturangenGrupp1.GUI
             sink[0] = "          ";
             sink[1] = "          ";
 
-            string[] witerActivity = new string[GenerateObjects._waiters.Count];
+            string[] waiterActivity = new string[GenerateObjects._waiters.Count];
             for (int i = 0; i < GenerateObjects._waiters.Count; i++)
             {
-                string first = GenerateObjects._waiters[i].CleaningTable ? GenerateObjects._waiters[i].Name + " städer bordet [" + GenerateObjects._waiters[i].TimeActivity  +"]": GenerateObjects._waiters[i].Name + " jobbar hårt ";
-                witerActivity[i] = first;
+                
+                waiterActivity[i] = GenerateObjects._waiters[i].CleaningTable ? GenerateObjects._waiters[i].Name + " städar bordet. [" + GenerateObjects._waiters[i].TimeActivity + "]" : 
+                                   GenerateObjects._waiters[i].Name + " jobbar hårt.";
 
             }
 
@@ -259,7 +260,7 @@ namespace ResturangenGrupp1.GUI
             for (int i = 0; i < GenerateObjects._chefs.Count; i++)
             {
                 string first = GenerateObjects._chefs[i].Competence > 3 ? "Stjärnkocken " + GenerateObjects._chefs[i].Name : "Kocken " + GenerateObjects._chefs[i].Name;
-                string second = GenerateObjects._chefs[i].Busy ? " lagar mat. [" + GenerateObjects._chefs[i].TimeActivity + "]" : " latar sig";
+                string second = GenerateObjects._chefs[i].Busy ? " lagar mat. [" + GenerateObjects._chefs[i].TimeActivity + "]" : " latar sig.";
                 chefsActivity[i] = first + second;
             }
             Helper.Eraser(80, 25, 46);
@@ -286,7 +287,7 @@ namespace ResturangenGrupp1.GUI
             Console.SetCursorPosition(66, 25);
             Console.WriteLine("Dagens dricks = " + Math.Round(Eventhandler.Tips), 2);        
             Window.Draw("Chefs", 66, 39, chefsActivity);
-            Window.Draw("Waiters", 66, 30, witerActivity);
+            Window.Draw("Waiters", 66, 30, waiterActivity);
         }
     }
 }
